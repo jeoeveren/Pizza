5d2d638ad71edcbe93ffbb9bea7675f330672006
jeoeveren <jeroenvanoeveren@hotmail.com>
2017-06-29 16:31:44 +0200

    Pizza final version

--- menu.rb
+++ menu.rb
@@ -21,4 +21,11 @@ class Menu
     end
   end
 
+  def make_choice
+    puts "Which one would you like?"
+    pizza_number = gets.chomp.to_i
+
+    @pizzas.find { |pizza| pizza.number == pizza_number }
+  end
+
 end
---
+++ order.rb
@@ -0,0 +1,26 @@
+#order.rb
+
+class Order
+  attr_accessor :content
+  def initialize
+    # default value of content is an empty Array.
+    @content = []
+  end
+  # add a pizza to the order
+  def add(pizza)
+    @content << pizza
+  end
+
+  def value
+    @content.reduce(0){|sum,pizza| sum + pizza.price }
+  end
+
+  def print
+    puts "Your order is:"
+    puts "------------------"
+    @content.each do |pizza|
+      puts "#{pizza.name} - #{pizza.price}"
+    end
+    puts "------------------"
+  end
+end
